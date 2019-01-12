3b5e46d80f4d93df527b3b7fc555e751d8fdfcb1
Félix Noiseux <rap.felix@hotmail.com>
2019-01-12 17:31:09 -0500

    Bouton update website

---
+++ WebSite1/WebSite1/Settings/index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Felix Noiseux</title>
+</head>
+<body>
+<!--Tu as pas d'affaire ici-->
+<!--Plz stay away-->
+<?php
+if ($_GET['run']) {
+  # This code will run if ?run=true is set.
+  exec("/home/pi/Desktop/UpdateWebsite.sh");
+}
+?>
+
+<a href="?run=true">Mettre à jour le contenu du site web.</a>
+</body>
+</html>
